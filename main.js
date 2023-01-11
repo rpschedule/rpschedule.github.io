1582e0f53c8f1674923357fa1009b32c98ec672f
Grom <person.japescapes@gmail.com>
2023-01-10 20:11:03 -0600

    Menu button functionality

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function openMenu () {
+    document.getElementById('menu').classList.remove('hidden');
+}
+
+function closeMenu () {
+    document.getElementById('menu').classList.add('hidden')
+}
