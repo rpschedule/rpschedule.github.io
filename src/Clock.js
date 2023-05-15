ff40db9e589fe7ab818d44744218194d38b6a7f2
rpschedule <103065303+rpschedule@users.noreply.github.com>
2023-05-15 17:33:56 +0000

    reworking

--- src/Clock.js
+++ src/Clock.js
@@ -1,33 +1,48 @@
 import { Schedule } from './schedule.js' 
 
 export default function Clock () {
-    const time = new Date( Date.now() );
-
-    for ( let i = 0; i < 365; i++ ) {
-        const compare = new Date(time.getFullYear(), time.getMonth(), time.getDate() + i, time.getMinutes(), time.getSeconds(), time.getMilliseconds());
-        if ( daysOffDateObjects( Schedule.YEAR_SCHEDULE ).filter(date => date.getTime() > time.getTime())[0].getDate() !== time.getDate() && !(time.getDay() === 0 || time.getDay === 6)) {
-            
-        }
-    }
 }
 
-// todo: memoize
-/**
- * Returns an array with a list of dates that school is off from the provided schedule
- */
-function daysOffDateObjects ( yearSchedule ) {
-    let out = [];
-    const time = new Date( Date.now() )
-
-    for ( const month in yearSchedule ) {
-        for ( const dayIndex in yearSchedule[month].offdays ) {
-            if ( month >= time.getMonth && yearSchedule[month].offdays[dayIndex] >= time.getDay() ) {
-                out.push(new Date( time.getFullYear(), month, yearSchedule[month].offdays[dayIndex]));
-            } else {
-                out.push(new Date( time.getFullYear() + 1, month, yearSchedule[month].offdays[dayIndex]));
-            } 
-        }
+function getNextBlock () {
+    const now = new Date(Date.now);
+    let nextSchoolDayIndex;
+    for ( let i = 0; i < 365 && !nextSchoolDay; i++ ) {
+        const compDate = new Date ( now.getTime() + 86400000 * i );
+        if ( compDate.isSchoolDay() ) nextSchoolDay = compDate;
     }
 
-    return out;
+}
+
+/**
+ * Whether or not it is a school day
+ * @param {object} schedule - The schedule to 
+ * @returns {boolean} 
+ */
+Date.prototype.isSchoolDay = (schedule) => {
+    return !schedule.YEAR_SCHEDULE[this.getMonth()].offdays.includes(this.getDate());
+}
+
+/**
+ * Whether or not it is a half day
+ * @param {object} schedule - The schedule to 
+ * @returns {boolean} 
+ */
+Date.prototype.isHalfDay = (schedule) => {
+    return !schedule.YEAR_SCHEDULE[this.getMonth()].halfdays.includes(this.getDate());
+}
+
+/**
+ * Returns which schedule to use based on the day of the week
+ * 0 - Monday, Wednesday, Friday
+ * 1 - Tuesday, Thursday
+ * 2 - Halfday
+ * null - Not a schoolday
+ * @returns {number} schedule - 0-2 indicating which schedule to use
+ */
+Date.prototype.schedule = () => {
+    if ( !this.isSchoolDay ) return null;
+    if ( this.isHalfDay() ) return 2;
+    if ( [2, 4, 6].includes(this.getDay()) ) return 0;
+    if ( [3, 5].includes(this.getDay()) ) return 1;
+    return null;
 }
