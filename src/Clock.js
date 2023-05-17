427c3df61514f94389af67f50b41a42deddd6ec1
rpschedule <103065303+rpschedule@users.noreply.github.com>
2023-05-17 17:20:46 +0000

    added updating

--- src/Clock.js
+++ src/Clock.js
@@ -1,32 +1,43 @@
-import { Schedule } from './schedule.js' 
+import { Schedule } from './schedule.js'
+import { useState, useEffect } from 'react';
 
-export default function Clock ({showWeeks, showMilliseconds}) {
-    const nextBlock = getNextBlock();
+export default function Clock({ showWeeks, showMilliseconds }) {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const nextBlock = getNextBlock(time);
     let ttnb = nextBlock.getTime() - Date.now();
 
     // convert to weeks, days, hours, minutes, and seconds (possibly milliseconds too) then display to user
     let weeks;
     if (showWeeks) {
-        weeks = Math.floor(ttnb/604800000)
+        weeks = Math.floor(ttnb / 604800000)
         ttnb -= weeks * 604800000;
     }
 
-    const days = Math.floor(ttnb/86400000);
+    const days = Math.floor(ttnb / 86400000);
     ttnb -= days * 86400000;
 
-    const hours = Math.floor(ttnb/3600000);
+    const hours = Math.floor(ttnb / 3600000);
     ttnb -= hours * 3600000;
 
-    const minutes = Math.floor(ttnb/60000);
+    const minutes = Math.floor(ttnb / 60000);
     ttnb -= minutes * 60000;
 
-    console.log(Math.floor(ttnb/1000))
+    console.log(Math.floor(ttnb / 1000))
 
-    const seconds = Math.floor(ttnb/1000);
+    const seconds = Math.floor(ttnb / 1000);
     ttnb -= seconds * 1000;
 
     // at this point, ttnb is the number of milliseconds
-    
+
     let result;
 
     if (showWeeks && weeks !== 0) {
@@ -47,20 +58,19 @@ export default function Clock ({showWeeks, showMilliseconds}) {
 }
 
 // make this also return the index of the block so Clock knows what event is next
-function getNextBlock () {
-    const now = new Date(Date.now());
-    const schedule = getSchedule(now) === 0 ? Schedule.MON_WED_FRI : 
+function getNextBlock(now) {
+    const schedule = getSchedule(now) === 0 ? Schedule.MON_WED_FRI :
         getSchedule(now) === 1 ? Schedule.TUES_THUR :
-        getSchedule(now) === 2 ? Schedule.HALF_DAY : null; 
+            getSchedule(now) === 2 ? Schedule.HALF_DAY : null;
     let nextSchoolDay;
 
     let lastDayWasSchoolDay = false;
-    for ( let i = 0; i < 365 && !nextSchoolDay; i++ ) {
-        const compDate = new Date ( now.getTime() + 86400000 * i );
+    for (let i = 0; i < 365 && !nextSchoolDay; i++) {
+        const compDate = new Date(now.getTime() + 86400000 * i);
         const lastBlock = new Date(compDate.getFullYear(), compDate.getMonth(), compDate.getDate(), schedule[schedule.length - 1][0], schedule[schedule.length - 1][1], schedule[schedule.length - 1][2]);
 
-        if ( isSchoolDay(compDate) && (compDate.getTime() <= lastBlock.getTime() || lastDayWasSchoolDay) ) nextSchoolDay = new Date(compDate);
-        if ( isSchoolDay(compDate)) lastDayWasSchoolDay = true;
+        if (isSchoolDay(compDate) && (compDate.getTime() <= lastBlock.getTime() || lastDayWasSchoolDay)) nextSchoolDay = new Date(compDate);
+        if (isSchoolDay(compDate)) lastDayWasSchoolDay = true;
     }
 
     let blocks = []
@@ -72,11 +82,11 @@ function getNextBlock () {
 
         // new Date(year, monthIndex, day, hours, minutes, seconds)
         blocks.push(new Date(
-            nextSchoolDay.getFullYear(), 
-            nextSchoolDay.getMonth(), 
-            nextSchoolDay.getDate(), 
-            hours, 
-            minutes, 
+            nextSchoolDay.getFullYear(),
+            nextSchoolDay.getMonth(),
+            nextSchoolDay.getDate(),
+            hours,
+            minutes,
             seconds
         ));
     }
@@ -90,7 +100,7 @@ function getNextBlock () {
  * @param {date} date - The date to use
  * @returns {boolean} 
  */
-function isSchoolDay (date) {
+function isSchoolDay(date) {
     return !Schedule.YEAR_SCHEDULE[date.getMonth()].offdays.includes(date.getDate());
 }
 
@@ -100,7 +110,7 @@ function isSchoolDay (date) {
  * @param {date} date - The date to use
  * @returns {boolean} 
  */
-function isHalfDay (date) {
+function isHalfDay(date) {
     return Schedule.YEAR_SCHEDULE[date.getMonth()].halfdays.includes(date.getDate());
 }
 
@@ -113,12 +123,12 @@ function isHalfDay (date) {
  * @param {date} date - The date to use
  * @returns {number} schedule - 0-2 indicating which schedule to use
  */
-function getSchedule (date) {
-    if ( !isSchoolDay(date) ) return null;
-    if ( isHalfDay(date) ) return 2;
-    if ( [1, 3, 5].includes(date.getDay()) ) return 0;
-    if ( [2, 4].includes(date.getDay()) ) return 1;
+function getSchedule(date) {
+    if (!isSchoolDay(date)) return null;
+    if (isHalfDay(date)) return 2;
+    if ([1, 3, 5].includes(date.getDay())) return 0;
+    if ([2, 4].includes(date.getDay())) return 1;
     return null;
 
-    
+
 }
