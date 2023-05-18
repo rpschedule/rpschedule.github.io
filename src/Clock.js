92fb2d36780aba21381b3b0e64ff5aeada56601a
Grom <93106270+lGrom@users.noreply.github.com>
2023-05-17 19:25:35 -0500

    fully functional with styling

--- src/Clock.js
+++ src/Clock.js
@@ -40,8 +40,6 @@ export default function Clock({ showWeeks }) {
 
     let result;
 
-    console.log(seconds)
-
     if (showWeeks && weeks !== 0) {
         result = `${weeks}:${days}:${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
     } else {
