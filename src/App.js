374ca7bed6952cf87628df0a6fc80386d71591a1
rpschedule <103065303+rpschedule@users.noreply.github.com>
2023-05-18 09:17:12 -0500

    todo: finish bottom css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className='App'>
       <Clock showWeeks={true} showMilliseconds={true} />
+      <div className='Bottom'>
+        <p>Questions? Check the <a href='https://github.com/rpschedule/rpschedule.github.io'>repo</a></p>
+      </div>
     </div>
   );
 }
