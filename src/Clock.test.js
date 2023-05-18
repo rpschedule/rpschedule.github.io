2be19f84b2aa4a754566fc5f2270ecd26ef41a4e
rpschedule <103065303+rpschedule@users.noreply.github.com>
2023-05-18 14:33:14 -0500

    built to prod

--- src/Clock.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Clock from './Clock';
-
-test('renders event', () => {
-  render(<Clock />);
-  const linkElement = screen.getByText(/until/i);
-  expect(linkElement).toBeInTheDocument();
-});
